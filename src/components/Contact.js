8bb479fa274c8ab3404a89677a87438ebc53b799
tolmamal <tolmamal@gmail.com>
2020-10-28 16:13:18 +0200

    contact

---
+++ src/components/Contact.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+import { Paper, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        height: 400,
+    }
+}));
+
+
+const Contact = () => {
+    const classes = useStyles();
+
+    return (
+        <Paper className={classes.root}>
+            <Typography variant="h2" color="primary">
+                Resume
+            </Typography>
+        </Paper>
+    );
+
+
+};
+
+export default Contact;
